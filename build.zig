804dbe1d68625b9a6c8532b7a036124cdefd2018
Francis Eugene Casibu <projectnevz@gmail.com>
2025-08-22 20:15:06 +0800

    add manifest file

--- build.zig
+++ build.zig
@@ -6,19 +6,19 @@ pub fn build(b: *std.Build) void {
 
     const lib = b.addStaticLibrary(.{
         .name = "json",
-        .root_source_file = b.path("json.zig"),
+        .root_source_file = b.path("src/json.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     lib.addCSourceFile(.{
-        .file = b.path("lib/stb_wrap.c"),
+        .file = b.path("src/lib/stb_wrap.c"),
     });
 
     lib.linkLibC();
     b.installArtifact(lib);
 
-    const json_mod = b.addModule("json", .{ .root_source_file = b.path("json.zig"), .target = target, .optimize = optimize });
+    const json_mod = b.addModule("json", .{ .root_source_file = b.path("src/json.zig"), .target = target, .optimize = optimize });
 
-    json_mod.addIncludePath(b.path("lib"));
+    json_mod.addIncludePath(b.path("src/lib"));
 }
